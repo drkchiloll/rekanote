31c7fdb2adfcae4a69e0cce4497334e94a511c86
drkchiloll <samwomack_@me.com>
2015-10-15 15:25:35 -0500

    moved notes into the react app as state data

--- app/components/App.js
+++ app/components/App.js
@@ -2,19 +2,24 @@ import React from 'react';
 import Notes from './Notes';
 import uuid from 'node-uuid';
 
-const notes = [{
-  id : uuid.v4(),
-  task: 'Learn Webpack'
-}, {
-  id : uuid.v4(),
-  task : 'Learn React'
-}, {
-  id : uuid.v4(),
-  task : 'Do laundry'
-}];
-
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes : [{
+        id : uuid.v4(),
+        task: 'Learn Webpack'
+      }, {
+        id : uuid.v4(),
+        task : 'Learn React'
+      }, {
+        id : uuid.v4(),
+        task : 'Do laundry'
+      }]
+    };
+  }
   render() {
+    const notes = this.state.notes;
     return (
       <div>
         <Notes items={notes} />
