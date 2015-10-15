30c3bb5ee0dcfe83a4d666defd16812c1cc12843
drkchiloll <samwomack_@me.com>
2015-10-15 08:44:32 -0500

    basic webpack setup with devServer, css loading, etal

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var HtmlwebpackPlugin = require('html-webpack-plugin'),
+    webpack = require('webpack'),
+    merge = require('webpack-merge'),
+    path = require('path');
+
+var TARGET = process.env.npm_lifecycle_event,
+    ROOT_PATH = path.resolve(__dirname),
+    APP_PATH = path.resolve(ROOT_PATH, 'app'),
+    BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+var common = {
+  entry : APP_PATH,
+  output : {
+    path : BUILD_PATH,
+    filename : 'bundle.js'
+  },
+  devServer : {
+    historyApiFallback : true,
+    port : 3000,
+    hot : true,
+    inline : true,
+    progress : true
+  },
+  module : {
+    loaders : [{
+      test : /\.css$/,
+      loaders : ['style','css'],
+      include : APP_PATH
+    }]
+  },
+  plugins : [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlwebpackPlugin({ title : 'WebPack Starter'})
+  ]
+};
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool : 'eval-source-map',
+    devServer : {
+      historyApiFallback : true,
+      hot : true,
+      inline : true,
+      progress : true
+    },
+    plugins : [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
