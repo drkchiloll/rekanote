41ffc7f17a05725ecc4f9eef659732522e432019
drkchiloll <samwomack_@me.com>
2015-10-15 15:26:13 -0500

    added classNames in Notes component for future styling purposes

--- app/components/Notes.js
+++ app/components/Notes.js
@@ -4,14 +4,15 @@ import Note from './Note';
 export default class Notes extends React.Component {
   renderNote(note) {
     return (
-      <li key={note.id}>
+      <li className='note' key={note.id}>
         <Note task={note.task} />
       </li>
     );
   }
   render() {
+    const notes = this.props.items;
     return (
-      <ul>{this.props.items.map(this.renderNote)}</ul>
+      <ul className='notes'>{notes.map(this.renderNote)}</ul>
     );
   }
 }
