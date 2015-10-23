65865800342a04dec508568a59158319acbd9cda
drkchiloll <samwomack_@me.com>
2015-10-23 09:48:25 -0500

    add actions for create update and delete

--- app/actions/LaneActions.js
+++ app/actions/LaneActions.js
@@ -1,2 +1,4 @@
 import alt from '../libs/alt';
-export default alt.generateActions('create', 'attachToLane', 'deleteFromLane');
+export default alt.generateActions(
+  'create', 'update', 'delete',
+  'attachToLane', 'deleteFromLane');
